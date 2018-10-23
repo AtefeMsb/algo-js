71c3d73b66b2e137b0c190aeb1f6896963ec8750
Atefe Mosayebi <atefe.mosayebi@gmail.com>
2018-10-23 15:14:35 -0700

    Delete mediumFinder-recursive.js

--- COEN-279/mediumFinder-recursive.js
+++
@@ -1,8 +0,0 @@
-/**
- * Decrese-conqure Solution
- * Complexity: Theta(n^2)
- * The function returns the (left) median of an unsorted array.
- * @param {string|integer} arr | unsorted array of strings or numbers
- * @returns {string|integer} arr[i] | the median of the array.
- */
-
