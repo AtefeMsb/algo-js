51f347a26d38796855fe8e492639d59ccd2c76bd
Atefe Mosayebi <atefe.mosayebi@gmail.com>
2018-12-18 12:37:11 -0800

    creates a random permutation of an array

---
+++ LeetCode/shuffleAnArray.js
@@ -0,0 +1,65 @@
+/**
+ * constructor function
+ * @param {number[]} nums
+ */
+var Solution = function(nums) {
+    this.nums = nums;
+};
+
+/**
+ * Resets the array to its original configuration and return it.
+ * @return {number[]}
+ */
+Solution.prototype.reset = function() {
+    return this.nums;
+};
+
+/**
+ * Returns a random shuffling of the array.
+ * @return {number[]}
+ */
+Solution.prototype.shuffle = function() {
+
+    // use a Set to make sure each index has been picked just once
+    const set = new Set();
+    const shuffledArray = [];
+    for (let i = 0; i < this.nums.length; i++) {
+        let index;
+
+        // find random index that is not been picked before
+        do {
+            index = randomNumber(0, this.nums.length);
+        } while (set.has(index));
+
+        set.add(index);
+        shuffledArray[i] = this.nums[index];
+    }
+  
+    // returns a random permutation of the nums array
+    return shuffledArray;
+};
+
+/**
+ * returns a random number between min - max
+ * @return {integer}
+ */
+function randomNumber(min, max) {
+
+    // 0 <= num < 1
+    let num = Math.random();
+
+    num = Math.floor(num * (max - min)) + min;
+    return num;
+}
+/** 
+ * Your Solution object will be instantiated and called as such:
+ * var obj = Object.create(Solution).createNew(nums)
+ * var param_1 = obj.reset()
+ * var param_2 = obj.shuffle()
+ */
+
+let solution = new Solution([1, 2, 3, 4, 5]);
+let shuffledArray = solution.shuffle();
+
+console.log(shuffledArray);
+console.log(solution.reset());
